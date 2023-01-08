7083ef357370e4aa9c49379bc7c91b6349ed32a0
Keval Khatri <kevalkhatri32@gmail.com>
2023-01-08 22:47:06 +0530

    Basic Server Setup

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = 4000 || process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log("Server Started");
+});
