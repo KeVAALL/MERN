eb00a9115587fc8ce7004d2cb6fcb0a388956270
Keval Khatri <kevalkhatri32@gmail.com>
2023-01-08 23:11:37 +0530

    Add State to the form

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,41 @@
 import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div>
       <h1>Register</h1>
       <form>
-        <input type="text" placeholder="First name" />
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          placeholder="Name"
+        />
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          placeholder="Password"
+        />
+        <input type="submit" value="Register" />
       </form>
     </div>
   );
